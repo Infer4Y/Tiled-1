db1bdb933d152868b537b0902e7727663f62b2f5
XavierDD1st <xavierdillinger@gmail.com>
2019-11-06 18:53:28 -0500

    "Test 2"

---
+++ scripts/Portal2.gd
@@ -0,0 +1,18 @@
+extends Area
+
+
+onready var player = get_node("../player/KinematicBody")
+
+
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_Area_body_entered(body):
+	print (body)
+	print (player)
+	if body == player: get_tree().change_scene("res://levels/level1.tscn")
